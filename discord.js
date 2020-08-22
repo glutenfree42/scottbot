2dad46486f2aad4d749479474e8387f8f4ababe5
GlutenFree <70077053+glutenfree42@users.noreply.github.com>
2020-08-22 18:03:43 -0400

    initial work

---
+++ discord.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = 'h'; 
+
+client.once('ready', () => {
+     console.log('scott bot is online!') 
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix)  ||  message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(" ");
+    console.log (args)
+    const command = args.shift().toLowerCase()
+
+    if(command === 'i' || command === "ello" || command === 'ey'){
+        message.channel.send('hey all scott here!')
+    }
+});
+
+client.login("NzMwNDUwNDMzMTkzMDE3Mzg1.Xwc1yQ.3xbiVToeQMszCaajEXhHW_OX2fg");
